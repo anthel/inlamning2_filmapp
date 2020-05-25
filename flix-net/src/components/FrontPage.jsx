ffcf2d778d9d65136f1c0e7386971a97af773250
Emma Ekman <emma.ekman@hotmail.com>
2020-05-25 15:41:34 +0200

    fixed buttons on frontpage

--- flix-net/src/components/FrontPage.jsx
+++ flix-net/src/components/FrontPage.jsx
@@ -2,7 +2,6 @@ import React, { Component, useState, useEffect } from 'react';
 import { useSelector} from 'react-redux';
 import { Carousel } from 'antd';
 import 'antd/dist/antd.css';
-import layer from '../svg/layer.png';
 import StarIcon from '@material-ui/icons/Star';
 import Button from '@material-ui/core/Button';
 import withHttpRequests from '../hoc/withHttpRequests';
@@ -37,15 +36,24 @@ const useStyles = makeStyles((theme) => ({
 
 function FrontPage(props) {
   const classes = useStyles();
+  const [movies, setMovies] = useState([]);
+  const [watchList, setWatchList] = useState([]);
 
+  const usersRedux = useSelector(state => state.saveNewUserReducer)
   
-    const [movies, setMovies] = useState([]);
-    const [watchList, setWatchList] = useState([]);
+  useEffect(()=>{
+    if(usersRedux.authenticated === true) {
+      props.getActiveUser(usersRedux.username)
+      
+      .then(res => res.json())
+      .then((activeUser) => {
+        console.log(activeUser)
+          setWatchList(activeUser.watchList)
+      })
+    }
+    },[])
 
-    const usersRedux = useSelector(state => state.saveNewUserReducer)
-
-
- props.getCarousel()
+  props.getCarousel()
     .then(movie => setMovies(movie));
 
   const LinkToYouTube = (title) => {
@@ -59,21 +67,18 @@ function FrontPage(props) {
     props.removeFromWatchlist(movie, usersRedux.username)
   }
 
-
-    
     return (
+      
       <React.Fragment>
         <Carousel autoplay> 
         {movies !== null ? 
           movies.map((movie)=>{
+            const showRemove = watchList.find((film)=> film._id === movie._id)
             return(
               <div key={movie._id}>
                 <div className="browserPosterWrapper">
 
-                  {/* <div className="shadowOnPoster">  */}
-                    {/* <img src={layer} alt="layer" className="layer"/> */}
                     <img className="browserPoster" alt="poster" src={movie.Poster}/>
-                  {/* </div> */}
                   
                   <div className="browserInfo">
                     <h1 className="headerTitle">{movie.Title}</h1>
@@ -83,11 +88,15 @@ function FrontPage(props) {
                     <div className="rating">
                       <h3><StarIcon/> IMDB RATING: {movie.imdbRating}</h3>
                       <Button variant="contained" className={classes.youtube} onClick={() => LinkToYouTube(movie.Title)}>Watch Trailer</Button>
-                      {!usersRedux.authenticated ?
-                        <div className="wrapperButtons">
-                          <Button variant="contained" className={classes.add} onClick={() => addToWatchlist(movie)}>Add To Watchlist</Button>
-                          <Button variant="contained" className={classes.remove} onClick={() => removeFromWatchlist(movie)}>Remove from Watchlist</Button>
-                        </div>
+                      {usersRedux.authenticated ?
+                        <React.Fragment>
+                          {showRemove ? 
+                            <Button variant="contained" className={classes.remove} onClick={() => removeFromWatchlist(movie)}>Remove from Watchlist</Button>
+                          : 
+                            <Button variant="contained" className={classes.add} onClick={() => addToWatchlist(movie)}>Add To Watchlist</Button>
+                          }
+                      }
+                        </React.Fragment>
                       : null
                       }
                     </div>
