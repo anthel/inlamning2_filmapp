635f8c84de4c55ec85e3f0fe71a4524aae2ef45a
Emma Ekman <emma.ekman@hotmail.com>
2020-05-26 11:57:07 +0200

    frontpage buttons done

--- flix-net/src/components/FrontPage.jsx
+++ flix-net/src/components/FrontPage.jsx
@@ -6,6 +6,7 @@ import StarIcon from '@material-ui/icons/Star';
 import Button from '@material-ui/core/Button';
 import withHttpRequests from '../hoc/withHttpRequests';
 import { makeStyles } from '@material-ui/core/styles';
+import useForceUpdate from 'use-force-update';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -21,16 +22,16 @@ const useStyles = makeStyles((theme) => ({
   } ,
   remove: {
     backgroundColor: 'rgba(181,13,24,1)',
-    fontSize: '11px'
-
+    fontSize: '11px',
+    marginLeft: '10px',
   },
   add: {
     backgroundColor: '#009688',
     fontSize: '11px',
+    marginLeft: '10px'
   },
   youtube: {
     fontSize: '11px'
-
   }
 }))
 
@@ -38,6 +39,7 @@ function FrontPage(props) {
   const classes = useStyles();
   const [movies, setMovies] = useState([]);
   const [watchList, setWatchList] = useState([]);
+  const [update, setUpdate] = useState(false);
 
   const usersRedux = useSelector(state => state.saveNewUserReducer)
   
@@ -51,7 +53,7 @@ function FrontPage(props) {
           setWatchList(activeUser.watchList)
       })
     }
-    },[])
+    },[update])
 
   props.getCarousel()
     .then(movie => setMovies(movie));
@@ -60,11 +62,20 @@ function FrontPage(props) {
     window.open("https://www.youtube.com/results?search_query="+ `trailer ${title}`); 
   }
 
-  const addToWatchlist = (movie) =>{
+  const addToWatchlist = (movie, add) =>{
     props.addToWatchlist(movie, usersRedux.username)
+    .then((response)=>{
+      setUpdate(!add)
+  
+      })
   }
-  const removeFromWatchlist = (movie) =>{
+  const removeFromWatchlist = (movie, remove) =>{
+
     props.removeFromWatchlist(movie, usersRedux.username)
+    .then((response)=>{
+    setUpdate(!remove)
+
+    })
   }
 
     return (
@@ -87,18 +98,21 @@ function FrontPage(props) {
                     <p className="actors">{movie.Actors}</p>
                     <div className="rating">
                       <h3><StarIcon/> IMDB RATING: {movie.imdbRating}</h3>
-                      <Button variant="contained" className={classes.youtube} onClick={() => LinkToYouTube(movie.Title)}>Watch Trailer</Button>
-                      {usersRedux.authenticated ?
-                        <React.Fragment>
-                          {showRemove ? 
-                            <Button variant="contained" className={classes.remove} onClick={() => removeFromWatchlist(movie)}>Remove from Watchlist</Button>
-                          : 
-                            <Button variant="contained" className={classes.add} onClick={() => addToWatchlist(movie)}>Add To Watchlist</Button>
-                          }
-                      }
-                        </React.Fragment>
-                      : null
-                      }
+                      <div className="wrapperButtons">
+                        <Button size="small" variant="contained" className={classes.youtube} onClick={() => LinkToYouTube(movie.Title)}>Watch Trailer</Button>
+                        {usersRedux.authenticated ?
+                          <React.Fragment>
+                            {showRemove ? 
+                              <Button size="small" variant="contained" className={classes.remove} onClick={() => removeFromWatchlist(movie, update)}>Remove from Watchlist</Button>
+                            : 
+                              <Button size="small" variant="contained" className={classes.add} onClick={() => addToWatchlist(movie, update)}>Add To Watchlist</Button>
+                            }
+                        }
+                          </React.Fragment>
+                        : null
+                        }
+                      </div>
+                      
                     </div>
                   </div>
 
