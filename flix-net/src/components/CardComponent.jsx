8f46112f2eec42ca56ab3239d185755cae3e0f62
Oscar Lindberg <oscarlindberg@me.com>
2020-05-27 14:20:17 +0200

    added btns for movies, removed red border

--- flix-net/src/components/CardComponent.jsx
+++ flix-net/src/components/CardComponent.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-/* 
+
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
-import ThumbDownIcon from '@material-ui/icons/ThumbDown'; */
+import ThumbDownIcon from '@material-ui/icons/ThumbDown'; 
 
 import withHttpRequests from '../hoc/withHttpRequests'
 import Button from '@material-ui/core/Button';
@@ -50,45 +50,48 @@ class CardComponent extends Component {
 
 
     return (
-      <div className="movie-card" onMouseEnter={this.showMoreInfo} onMouseLeave={this.hideInfo}>
-
-        <div className="movie-card-image" style={this.state.showInfo ? this.onHoverCard : null}>
-          <img alt="poster" src={this.props.movie.Poster}/>
-        </div>
-
-        {this.state.showInfo === true &&
-        <div className="cardWrapper">
-          <div className="movie-information" key={this.props.movie._id}>
-            <div>
-              <h1>{this.props.movie.Title}</h1>
-              <h3>Plot:<br /> {this.props.movie.Plot}</h3>
-            </div>
-            <div className="genre-cast-bar">
-              <h4>Genre: {this.props.movie.Genre}</h4>
-              <h4>Cast: {this.props.movie.Actors}</h4>
-              {this.props.usersRedux.authenticated ? 
-                <>
-                {this.props.showRemove ?
-                  <Button variant="contained" onClick={() => this.removeFromWatchlist(this.props.movie)}>Remove from Watchlist</Button>
-                  : 
-                  <Button variant="contained" onClick={() => this.addToWatchlist(this.props.movie)}>Add To Watchlist</Button>}}
-                </> : null
-              }
-
-
-            {/* <div id="thumbContainer">
-              <ThumbUpIcon style={{fontSize:"6em", color: "white"}}/>
-              <ThumbDownIcon style={{fontSize:"6em", color: "white"}}/>
-            </div> */}
-
-     
-            </div>
+      <div className="cardWrapper">
+        <div className="movie-card" onMouseEnter={this.showMoreInfo} onMouseLeave={this.hideInfo}>
+          <div className="movie-card-image" style={this.state.showInfo ? this.onHoverCard : null}>
+            <img alt="poster" src={this.props.movie.Poster}/>
           </div>
+          {this.state.showInfo === true &&
+            <div className="movie-information" key={this.props.movie._id}>
+              <div>
+                <h1>{this.props.movie.Title}</h1>
+                <h3>Plot:<br /> {this.props.movie.Plot}</h3>
+              </div>
+              <div className="genre-cast-bar">
+                <h4>Genre: {this.props.movie.Genre}</h4>
+                <h4>Cast: {this.props.movie.Actors}</h4>
+              </div>
+            </div>
+          }
+        </div>    
+        <div className="buttonWrapper"></div>
+          <div id="thumbContainer">
+            <ThumbUpIcon style={{fontSize:"4em", color: "white"}}/>
+            <ThumbDownIcon style={{fontSize:"4em", color: "white"}}/>
+          </div> 
+          {this.props.usersRedux.authenticated ? 
+            <>
+            {this.props.showRemove ?
+              <Button variant="contained" className="movie-card-button" onClick={() => this.removeFromWatchlist(this.props.movie)}>Remove from Watchlist</Button>
+              : 
+              <Button variant="contained" className="movie-card-button" onClick={() => this.addToWatchlist(this.props.movie)}>Add To Watchlist</Button>}
+            </> : null
+          }
         </div>
-        }
-      </div>
+
     )
   }
 }
 
 export default withHttpRequests(CardComponent)
+
+
+
+
+
+
+
